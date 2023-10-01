e0725926ab7e000b85e867c22b992afa024a6d21
Daniel Weninger <dweninger315@gmail.com>
2023-10-01 16:47:19 -0400

    changed password generator to convert user input to int

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,7 +1,10 @@
 import string
 import random
 
-possible_chars = [string.ascii_uppercase, string.ascii_lowercase, string.digits, "!@#$%^&*"]
+possible_chars = [string.ascii_uppercase, 
+                  string.ascii_lowercase, 
+                  string.digits, 
+                  "!@#$%^&*"]
 
 def get_password(len):
     # make sure length entered is a number greater than 3
@@ -15,7 +18,9 @@ def get_password(len):
         remaining_len = len - 4
         # fill in remaining characters for the password
         for _ in range(remaining_len):
-            random_password += random.choice(random.choice(possible_chars))
+            random_password += random.choice(
+                random.choice(possible_chars)
+                )
         # scramble the password characters
         random_password_list = list(random_password)
         random.shuffle(random_password_list)
@@ -23,4 +28,5 @@ def get_password(len):
         return random_password
 
 password_length = input("Enter password length: ")
+password_length = int(password_length)
 print(get_password(password_length))
